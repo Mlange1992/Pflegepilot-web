bb6a6798c72e40ed654a49bedc68e4d14703e480
Markus Lange <lange_m@me.com>
2026-04-27 22:16:44 +0200

    feat(app-store): finale Apple-ID 6764144722 eintragen

--- lib/constants/app-store.ts
+++ lib/constants/app-store.ts
@@ -1,4 +1,2 @@
-// TODO: Sobald die App Store ID feststeht, hier eintragen.
-// Quelle: App Store Connect → Apps → PflegePilot → App-Informationen → "Apple-ID".
-// Pattern: https://apps.apple.com/de/app/pflegepilot/id{ID}
-export const APP_STORE_URL = 'https://apps.apple.com/de/app/pflegepilot/id0000000000'
+// Apple-ID aus App Store Connect → App-Informationen.
+export const APP_STORE_URL = 'https://apps.apple.com/de/app/pflegepilot/id6764144722'
